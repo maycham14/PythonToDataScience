0b1581bdd54a60ce3b642e74e8f9c8cdd5d87f93
Mariama Cham <mayscham14@gmail.com>
2022-08-19 14:53:24 +0000

    Update

---
+++ function.py
@@ -0,0 +1,12 @@
+def display_message():
+    print("I am learning something new")
+
+
+display_message()
+
+
+def favorite_book(title):
+    print(f"my favorite book is {title.title()}")
+
+
+favorite_book("amadu")
