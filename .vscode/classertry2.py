127ea70143cfb18e889401e5de950047d5dc0f0e
Mariama Cham <mayscham14@gmail.com>
2022-09-02 11:53:16 +0000

    Update

--- .vscode/classertry2.py
+++ .vscode/classertry2.py
@@ -2,13 +2,12 @@ class IceCreamStand:
 
     def __init__(self, restaurant_name, cuisine_type):
         super().__init__(self, restaurant_name, cuisine_type)
-        self.flavors = ["chocolate","vanilla","orange","coffee"]
+        self.flavors = ["chocolate", "vanilla", "orange", "coffee"]
 
     def ice_cream_flavors(self):
         for i in self.flavors:
             print(f"flavors are {i}")
 
 
-my = IceCreamStand("place","gambian")
-print(my.describe_restaurant())
-my.IceCreamStand.ice_cream_flavors()
+my = IceCreamStand("place", "gambian")
+my.ice_cream_flavors()
