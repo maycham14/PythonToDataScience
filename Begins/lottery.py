0487f917e01e74462286b7e31512e5d60d71da1a
Mariama Cham <mayscham14@gmail.com>
2022-09-16 13:17:16 +0000

    Doing OOP in Python

--- Begins/lottery.py
+++ Begins/lottery.py
@@ -1,5 +1,31 @@
 import random
 
-letters = [1, 2, 3, 4, 5, "a", "b", "c", "d", "e"]
-chosen = random.choice(str(letters))
- 
+
+class Lottery:
+
+    def __init__(self):
+        self.letters = [1, 2, 3, 4, 5, "a", "b", "c", "d", "e"]
+        self.guess= []
+
+    def select_nth_items(self, num):
+        for i in range(num):
+            index = random.randint(0, len(self.letters) - 1)
+            self.guess.append(self.letters[index])
+
+        return self.guess
+
+    def return_format(self):
+        output = ""
+        for char in self.guess:
+            if type(char) == int:
+                output += str(char)
+            else:
+                output += char
+
+        print(f"This is the lottery output \"{output}\"")
+
+
+
+lotto = Lottery()
+lotto.select_nth_items(7)
+lotto.return_format()
